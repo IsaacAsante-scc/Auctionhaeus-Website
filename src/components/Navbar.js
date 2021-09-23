16282d34e08864a0df1a817d03139bd536bdd5da
Isaac Asante <W0725009@myscc.ca>
2021-09-23 02:24:43 -0400

    Add Navbar CSS
    Added navbar css and fixed sign up button for mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,25 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MdFingerprint } from 'react-icons/md'
 import { FaBars, FaTimes } from 'react-icons/fa'
+import { Button } from './Button'
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click)
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize', showButton);
 
 
     return (
@@ -44,7 +58,9 @@ function Navbar() {
                             ): (
                                 <Link to='/sign-up' className='btn-link'>
                                     <Button buttonStyle='btn--outline'
-                                        buttonSize='btn--mobile'>SIGN UP</Button>
+                                        buttonSize='btn--mobile'
+                                        onClick={closeMobileMenu}
+                                        >SIGN UP</Button>
                                 </Link>
                             )}
                         </li>
